99cd697132dd6bf982c2e1f648595597d6767df6
apo-mak <apo-mak@users.noreply.github.com>
2021-09-12 03:03:06 +0300

    add amd-ryzen-master

---
+++ amd-ryzen-master/tools/chocolateyinstall.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$procName = (Get-WmiObject Win32_Processor).Name
+if (!$procName.Contains('Ryzen')) {
+    Write-Error 'Only compatible with AMD Ryzen processors'
+}
+
+$url = 'https://download.amd.com/Desktop/AMD-Ryzen-Master.exe'
+$checksum = '00d927489a38967b66d9ce7414c472aa91a56963fe9d857b4f4ac897cfddab76'
+$slientArgs = '/S /v/qn'
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    fileType       = 'EXE'
+    url            = $url
+    url64          = $url
+    softwareName   = 'amd-ryzen-master*'
+    checksum       = $checksum
+    checksumType   = 'sha256'
+    checksum64     = $checksum
+    checksumType64 = 'sha256'
+    silentArgs     = $slientArgs
+    silentArgs64   = $slientArgs
+    validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
